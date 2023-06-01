16183a50af45bbdb5388fcbc4cc6a46d1ccc0ad5
zhuhaodavid <zhuhao6590@outlook.com>
2023-06-01 22:51:12 +0800

    renamed:    BasicTools.py -> Basicfun.py

--- Basicfun.py
+++ Basicfun.py
@@ -1,3 +1,63 @@
+import scipy.io as sio
+
+
+def save_mat(path, H):
+    H = H.tocsr().tocoo()
+    H_coo = np.zeros((3, len(H.row)))
+    H_coo[0, :] = H.row
+    H_coo[1, :] = H.col
+    H_coo[2, :] = H.data
+    sio.savemat(path, {'H_coo': H_coo, 'dim': H.shape[0]})
+
+
+def save_hdf5(path, data):
+    # save data to HDF5
+    import h5py
+    with h5py.File(path+".h5", "w") as file:
+        file.create_dataset("data", data=data)
+
+
+def load_hdf5(path, int=None):
+    # read data to HDF5
+    import h5py
+    if path[-3:] != ".h5":
+        path += ".h5"
+    if int is None:
+        with h5py.File(path, "r") as file:
+            int = file["data"][()]  # returns as a numpy array
+    else:
+        with h5py.File(path, "r") as file:
+            file["data"].read_direct(int)
+    return int
+
+
+def martix_O_obs(psi, O):
+    return psi.transpose().conj() @ O.dot(psi)
+
+
+def Gauss_fun(mu, sigma2, xlist):
+    Gauss = 1/(2*np.pi*sigma2)**0.5 * np.exp(-(xlist - mu)**2/(2*sigma2))
+    return Gauss
+
+
+def log_Gauss_fun(mu, sigma2, xlist):
+    ylist = []
+    for xi in xlist:
+        yi = 1/(2*np.pi*sigma2)**0.5 * np.exp(-(np.log(abs(xi)) - mu)**2/(2*sigma2)) / abs(xi) / 2
+        ylist.append(yi)
+    return ylist
+
+
+def selection_Inx(array, left, right, inclu=True):
+    if inclu:
+        return (left <= array) * (array <= right)
+    else:
+        return (left < array) * (array < right)
+
+
+
+
+
 import torch as tc
 
 
